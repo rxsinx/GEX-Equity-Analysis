2d3eb76593fb5158256b9bc9ac361ef615ddf4cf
rxsinx <raghunath.nayak.pgd2020@iimu.ac.in>
2026-06-17 20:41:00 +0530

    Update equity_visualizations.py

--- modules/equity_visualizations.py
+++ modules/equity_visualizations.py
@@ -739,8 +739,8 @@ def plot_gex_oi_clustered(df_chain, selected_stock, spot_price, lower_bound, upp
             is_nearest = strike == nearest_strike
             fig.add_vline(
                 x=strike,
-                line=dict(color='#fbbf24' if is_nearest else '#9ca3af', width=2 if is_nearest else 1.3, dash='dash'),
-                annotation_text=f"{strike:,.0f}", 
+                line=dict(color='#fbbf24' if is_nearest else '#9ca3af', width=2 if is_nearest else 1.5, dash='dash'),
+                annotation_text=f"OI:Cross{strike:,.0f}", 
                 annotation_position="bottom left",
                 annotation_font=dict(size=10, color='#fbbf24' if is_nearest else '#9ca3af')
             )
@@ -749,8 +749,8 @@ def plot_gex_oi_clustered(df_chain, selected_stock, spot_price, lower_bound, upp
     # ------------------ SPOT PRICE LINE ------------------
     fig.add_vline(
         x=spot_price,
-        line=dict(color='#151617', width=1, dash='dash'), # Blue dashed line
-        annotation_text=f"  Spot:{spot_price:,.0f}",
+        line=dict(color='#151617', width=1.5, dash='dash'), # Blue dashed line
+        annotation_text=f"  SPOT:{spot_price:,.0f}",
         annotation_position="top right",
         annotation_font=dict(size=10, color='#60a5fa')
     )
